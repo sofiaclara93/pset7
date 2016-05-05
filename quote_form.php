513789501a3ea2b9ab9d17ba51a7a1db89c2435f
sofiaclara93 <sofiaclara93@gmail.com>
2016-05-04 21:46:28 -0400

    Create quote_form.php

---
+++ quote_form.php
@@ -0,0 +1,7 @@
+<form action="quote.php" method="post">
+    <fieldset>
+        <div class="form-group">
+            <input autofocus class="form-control" name="symbol" placeholder="Enter a stock symbol" type="text"/>
+        </div>
+    </fieldset>
+</form>
