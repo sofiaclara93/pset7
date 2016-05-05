1f75f7247028d002db928cd40131a5527ce91fef
sofiaclara93 <sofiaclara93@gmail.com>
2016-05-04 21:48:44 -0400

    Create buy_form.php

---
+++ buy_form.php
@@ -0,0 +1,13 @@
+<form action="buy.php" method="post">
+    <fieldset>
+        <div class="form-group">
+            <input autofocus class="form-control" name="symbol" placeholder="Enter a stock symbol" type="text"/>
+        </div>
+        <div class="form-group">
+            <input class="form-control" name="shares" placeholder="Shares" type="number"/>
+        </div>
+        <div class="form-group">
+            <button type="submit" class="btn btn-default">BUY</button>
+        </div>
+    </fieldset>
+</form>
